cd8518ad26aef5b20e43d116b0a3cfbcc785bdc0
Johan Brook <johan.jvb@gmail.com>
2015-07-05 23:30:38 +0200

    ✅ Add version constraints

--- package.js
+++ package.js
@@ -8,6 +8,8 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
+  api.versionsFrom('1.0.4.2');
+
   api.use(['mongo', 'coffeescript', 'underscore'], both);
   api.use('templating', 'client');
 
