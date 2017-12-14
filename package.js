539477f36746536570ed3a78c61ffd8b15bad1a0
Johan Brook <johan@lookback.io>
2017-12-14 15:37:25 +0100

    Fix version in package.js

--- package.js
+++ package.js
@@ -3,7 +3,7 @@ var both = ['client', 'server'];
 Package.describe({
   name: 'lookback:emoji',
   summary: 'Easily render and manage emojis in Meteor.',
-  version: '0.3.3',
+  version: '0.3.4',
   git: 'https://github.com/lookback/meteor-emoji'
 });
 
